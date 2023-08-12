171e14ae76547775681f85731b789d8c0ade6d21
minconmincon <107973815+MinCOnMinCon@users.noreply.github.com>
2023-08-12 20:12:47 +0900

    Revert "캐릭터, 무기 - 공격, 상호작용 추가 및 개선 /"
    This reverts commit d683db71ca78587afe76b18ec2335fff6f61631c.

---
+++ Assets/Scripts/Character&Weapon/Consumable/testConsumable.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testConsumable : Consumable
+{
+    public testConsumable()
+    {
+        name = "testConsumable";
+    }
+
+    public override void effect()
+    {
+
+    }
+}
