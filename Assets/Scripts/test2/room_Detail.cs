a7e695e50a0575f670bfd3893e1e8e2fce2ba939
name <jackjsh@naver.com>
2023-08-22 21:01:30 +0900

    맵 디테일 카메라 경계 설정

--- Assets/Scripts/test2/room_Detail.cs
+++ Assets/Scripts/test2/room_Detail.cs
@@ -6,4 +6,12 @@ public class room_Detail : MonoBehaviour
 {
     public RoomType roomType;
     public GameObject room;
+
+    public Vector2 center;
+    public Vector2 size;
+
+    public void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(center, size);
+    }
 }
